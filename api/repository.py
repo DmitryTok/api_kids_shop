7135d6bacaed2587da0adce6f2a236b135a4de4b
Dmytro <86673991+DmitryTok@users.noreply.github.com>
2023-10-05 19:13:25 +0200

    Query refactor (#3)
    * refactor queries to database
    
    * refactor queries to database
    
    * refactor queries to database
    
    ---------
    
    Co-authored-by: DmitryTok <FoB09@ukr.net>

--- api/repository.py
+++ api/repository.py
@@ -1,4 +1,7 @@
+from django.db.models import Prefetch
+
 from api import models
+from api.models import InStock, Picture
 from kids_shop.base.base_repository import BaseRepository
 
 
@@ -8,11 +11,68 @@ class ProductRepository(BaseRepository):
     def model(self) -> type[models.Product]:
         return models.Product
 
+    def get_all_objects_order_by_id(self):
+        return self.model.objects.select_related(
+            'category',
+            'section',
+            'brand',
+            'discount'
+        ).prefetch_related(
+            Prefetch(
+                'in_stock',
+                queryset=InStock.objects.select_related(
+                    'product',
+                    'color',
+                    'product_size'
+                )
+            ),
+            Prefetch(
+                'product_images',
+                queryset=Picture.objects.select_related('product')
+            )
+        )
+
     def get_sorted_product_by_rate(self):
-        return self.model.objects.order_by('-rating')
+        return self.model.objects.select_related(
+            'category',
+            'section',
+            'brand',
+            'discount'
+        ).prefetch_related(
+            Prefetch(
+                'in_stock',
+                queryset=InStock.objects.select_related(
+                    'product',
+                    'color',
+                    'product_size'
+                )
+            ),
+            Prefetch(
+                'product_images',
+                queryset=Picture.objects.select_related('product')
+            )
+        ).order_by('-rating')
 
     def get_sorted_products_by_sale(self):
-        return self.model.objects.filter(is_sale=True)
+        return self.model.objects.filter(is_sale=True).select_related(
+            'category',
+            'section',
+            'brand',
+            'discount'
+        ).prefetch_related(
+            Prefetch(
+                'in_stock',
+                queryset=InStock.objects.select_related(
+                    'product',
+                    'color',
+                    'product_size'
+                )
+            ),
+            Prefetch(
+                'product_images',
+                queryset=Picture.objects.select_related('product')
+            )
+        )
 
 
 class SectionRepository(BaseRepository):
@@ -21,12 +81,18 @@ class SectionRepository(BaseRepository):
     def model(self) -> type[models.Section]:
         return models.Section
 
+    def get_all_objects_order_by_id(self):
+        return self.model.objects.values('id', 'name')
+
 
 class PictureRepository(BaseRepository):
 
     @property
     def model(self) -> type[models.Picture]:
         return models.Picture
+
+    def get_all_objects_order_by_id(self):
+        return self.model.objects.select_related('product')
 
 
 class CategoryRepository(BaseRepository):
