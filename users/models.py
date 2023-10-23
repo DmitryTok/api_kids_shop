99eaa5cacbab6e6350fbca5c64a04d5cbed53782
DmitryTok <FoB09@ukr.net>
2023-10-23 18:25:04 +0200

    signals for user profile

--- users/models.py
+++ users/models.py
@@ -3,8 +3,6 @@ from enum import Enum
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.core.validators import validate_email
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 class CustomUserManager(BaseUserManager):
@@ -146,30 +144,3 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.email
-
-
-@receiver(post_save, sender=CustomUser)
-def create_profile(sender, instance, **kwargs):
-    if not instance.is_superuser:
-        if not hasattr(instance, 'users_profile'):
-            user_profile = Profile(user=instance)
-            user_profile.save()
-
-
-@receiver(post_save, sender=Profile)
-def create_related_models(sender, instance, created, **kwargs):
-    if created:
-        kid = Kid.objects.create(male='Boy', birth_date=None)
-
-        address = Address.objects.create(
-            first_delivery_address='Your Address Here',
-            second_delivery_address='Optional Address',
-            city='Your City',
-            street='Your Street',
-            building='Your Building',
-            apartment=1
-        )
-
-        instance.kids.add(kid)
-        instance.address = address
-        instance.save()
